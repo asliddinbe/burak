339957dbca650c3c1cb279b6253a53e45e1923e2
asliddinbe <asliddinumbarov2002@gmail.com>
2024-09-04 23:14:02 +0900

    BRR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("EXUCUTED");
+
+const person: string = "Adam";
+const count: number = 2002;
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
