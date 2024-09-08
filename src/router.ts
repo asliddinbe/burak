0f4b69ce4b192236df03306353793bc662985532
asliddinbe <asliddinumbarov2002@gmail.com>
2024-09-09 01:22:23 +0900

    fix: modify routing system and controllers

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,6 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 import memberController from "./controollers/member.controller";
 
-router.get("/", memberController.goHome );
-
-router.get("/login", memberController.getLogin );
-
-router.get("/signup", memberController.getSignup );
 
 export default router;
 
