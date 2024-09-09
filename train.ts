d44b6a015621ecc2f5d8e28dbff048c4f82b8f21
asliddinbe <asliddinumbarov2002@gmail.com>
2024-09-10 03:28:42 +0900

    feat: create restaurant member related router get and post endpointd

--- train.ts
+++ train.ts
@@ -1,3 +1,20 @@
+/* Project Standards:
+- Logging standards:
+- Naming standards:
+       function, method, variable => CAMEL
+       class => PASCAL
+       folder => KEBAB
+       css => SNAKE
+
+-Error handling
+       */
+
+
+
+
+
+
+
 // // H2-Task
 
 function getNum(getStr: String): String {
