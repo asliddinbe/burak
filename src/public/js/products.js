e5aef874125ccad4765731051123abdc791be54a
asliddinbe <asliddinumbarov2002@gmail.com>
2024-10-03 16:30:51 +0900

    feat: update chosen product frontent logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -21,6 +21,25 @@ $(function () {
         $(".dish-container").slideToggle(100);
         $("#process-btn").css("display", "flex");
     });
+
+    $(".new-product-status").on("change", async function (e) {
+        const id = e.target.id;
+        const productStatus = $(`#${id}.new-product-status`).val();
+    
+        try {
+          const response = await axios.post(`/admin/product/${id}`, {
+            productStatus: productStatus,
+          });
+          console.log("response:", response);
+          const result = response.data;
+          if (result.data) {
+            $(".new-product-status").blur();
+          } else alert("Product update failed!");
+        } catch (err) {
+          console.log(err);
+          alert("Product update failed!");
+        }
+      });
 });
 
 function validateForm() {
