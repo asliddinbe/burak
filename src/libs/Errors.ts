077fc6ee4f3643447d741f9d9b5c2e03c7c7fc15
asliddinbe <asliddinumbarov2002@gmail.com>
2024-10-03 17:58:27 +0900

    fix: modify spa login rest api logic

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -17,6 +17,7 @@ export enum Message {
 
     USED_NICK_PHONE = "You are inserting already use nick or phone!",
     NO_MEMBER_NICK = "No member with that member nick!",
+    BLOCKED_USER = "You have been blocked!",
     WRONG_PASSWORD = "Wrong password , please try again!",
     NOT_AUTHENTICATED = "You are not authenicated, Please login first"
 }
